d70287a1f08edcfd742d0d7ca6f8e173455865ab
qu4ku <qu4ku@hotmail.com>
2018-01-02 08:02:45 +0100

    flask

--- flask-articles-dashboard/app.py
+++ flask-articles-dashboard/app.py
@@ -1,12 +1,26 @@
 from flask import Flask, render_template
+from data import Articles
 
 app = Flask(__name__)
 
+
+article_list = Articles()
 
 @app.route('/')
 def index():
 	return render_template('home.html')
 
+@app.route('/about')
+def about():
+	return render_template('about.html')
+
+@app.route('/articles')
+def articles():
+	return render_template('articles.html', articles=article_list)
+
+@app.route('/article/<string:id>/')
+def article(id):
+	return render_template('article.html', id=id)
 
 if __name__ == '__main__':
 	# debug=True : no need to restart the server every single time
---
+++ flask-articles-dashboard/data.py
@@ -0,0 +1,25 @@
+def Articles():
+	articles = [
+		{
+			'id': 1,
+			'title': 'Article One',
+			'body': 'china had 176 million surveillance cameras in operation last year and is expected to see that figure more than triple to reach 626 million by 2020.',
+			'author': 'qu4ku',
+			'create_date': '02-01-2018'
+		},
+		{
+			'id': 2,
+			'title': 'Article Two',
+			'body': 'china had 176 million surveillance cameras in operation last year and is expected to see that figure more than triple to reach 626 million by 2020.',
+			'author': 'qu4ku',
+			'create_date': '02-01-2018'
+		},
+		{
+			'id': 3,
+			'title': 'Article Three',
+			'body': 'china had 176 million surveillance cameras in operation last year and is expected to see that figure more than triple to reach 626 million by 2020.',
+			'author': 'qu4ku',
+			'create_date': '02-01-2018'
+		},
+	]
+	return articles
