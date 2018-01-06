e2fdc7e64b8ae2e3393095e0363a6f70a6e6038c
qu4ku <qu4ku@hotmail.com>
2018-01-06 17:34:23 +0100

    vue

---
+++ vue/vue-firestore-employeemanager/src/components/Navbar.vue
@@ -0,0 +1,9 @@
+<template>
+	<div id="navbar">
+		<div class="nav-wrap green">
+			<div class="container">
+				<router-link to="/" class="brand-logo">Employee Manager</router-link>
+			</div>
+		</div>
+	</div>
+</template>
