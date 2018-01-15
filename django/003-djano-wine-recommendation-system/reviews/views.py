d2c7cacc2ae2ec725e79c26d27b7fe021e1f5425
qu4ku <qu4ku@hotmail.com>
2018-01-15 14:53:28 +0100

    django

--- django/003-djano-wine-recommendation-system/reviews/views.py
+++ django/003-djano-wine-recommendation-system/reviews/views.py
@@ -3,13 +3,19 @@ from .models import Review, Wine
 
 # Create your views here.
 def review_list(request):
-	pass
+	latest_review_list = Review.objects.order_by('-pub_date')[:9]
+	context = {'latest_review_list': latest_review_list}
+	return render(request, 'review_list.html', context)
 
 def review_detail(request, review_id):
-	pass
+	review = get_object_or_404(Review, pk=review_id)
+	return render(request, 'review_detail.html', {'review': review})
 
 def wine_list(request):
-	pass
+	wine_list = Wine.objects.order_by('-name')
+	context = {'wine_list': wine_list}
+	return render(request, 'wine_list.html', context)
 
 def wine_detail(request, wine_id):
-	pass
+	wine = get_object_or_404(Wine, pk=wine_id)
+	return render(request, 'wine_detail.html', {'wine': wine})
