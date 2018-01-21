299eea6e9ee780990d721daf716502d939aeb322
qu4ku <qu4ku@hotmail.com>
2018-01-22 00:53:37 +0100

    django

---
+++ django/005-django-simple-app/pages/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.views.generic import TemplateView
+
+
+class HompePageView(TemplateView):
+	template_name = 'home.html'
+
+
+
+class AboutPageView(TemplateView):
+	template_name = 'about.html'
