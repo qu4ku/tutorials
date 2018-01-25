4268000cdbac57a81ca2ddeede318d5fe381dd98
qu4ku <qu4ku@hotmail.com>
2018-01-25 19:03:41 +0100

    django

--- django/008-django-social-auth/socialauth_project/settings.py
+++ django/008-django-social-auth/socialauth_project/settings.py
@@ -59,7 +59,7 @@ ROOT_URLCONF = 'socialauth_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -125,8 +125,8 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 AUTHENTICATION_BACKENDS = (
-    'django.contrib.auth.backends.ModelBackend',
-    'allauth.account.auth_backends.AuthenticationBackend',
+    "django.contrib.auth.backends.ModelBackend",
+    "allauth.account.auth_backends.AuthenticationBackend",
 )
 
 SITE_ID = 1
--- django/008-django-social-auth/socialauth_project/urls.py
+++ django/008-django-social-auth/socialauth_project/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
+    path('', views.HomePageView.as_view(), name='home'),
 ]
