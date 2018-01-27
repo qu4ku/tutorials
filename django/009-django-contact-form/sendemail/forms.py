8c685313894e781710d42ced5994265c626b31a0
qu4ku <qu4ku@hotmail.com>
2018-01-27 11:15:25 +0100

    django

--- django/009-django-contact-form/sendemail/forms.py
+++ django/009-django-contact-form/sendemail/forms.py
@@ -3,5 +3,5 @@ from django import forms
 
 class ContactForm(forms.Form):
 	from_email = forms.EmailField(required=True)
-	subjec = forms.CharField(required=True)
+	subject = forms.CharField(required=True)
 	message = forms.CharField(widget=forms.Textarea, required=True)
