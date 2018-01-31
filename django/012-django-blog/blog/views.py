90e0bfee086aa9d0434dfbc19098fe204de8e553
qu4ku <qu4ku@hotmail.com>
2018-01-31 15:36:35 +0100

    django

--- django/012-django-blog/blog/views.py
+++ django/012-django-blog/blog/views.py
@@ -2,13 +2,17 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from .models import Article
+
 
 def home(request):
+	
 	return render(request, 'homepage.html')
 
 def about(request):
 	return render(request, 'about.html')
 
 def article_list(request):
-	return render(request, 'article_list.html')
+	articles = Article.objects.all().order_by('date')
+	return render(request, 'article_list.html', {'articles': articles})
 
