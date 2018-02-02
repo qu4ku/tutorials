b257a1aa2d155c4f36a3c5bb3a2ede21544b9d27
qu4ku <qu4ku@hotmail.com>
2018-02-02 12:03:33 +0100

    django

--- django/012-django-blog/blog/urls.py
+++ django/012-django-blog/blog/urls.py
@@ -6,6 +6,10 @@ from . import views
 urlpatterns = [
 	path('', views.article_list, name='article_list'),
 	path('about/', views.about, name='about'),
+	# article/create needs to be above slug - if not slug will be fired first
+	path('article/create', views.article_create, name='article_create'),
 	path('article/<slug:slug>', views.article_detail, name='article_detail'),
-	path('accounts/signup/', views.signup_view, name='signup')
+	path('accounts/signup/', views.signup_view, name='signup'),
+	path('accounts/login/', views.login_view, name='login'),
+	path('accounts/logout/', views.logout_view, name='logout'),
 ]
--- django/012-django-blog/blog/views.py
+++ django/012-django-blog/blog/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
-from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import login, logout
 
 from .models import Article
 
@@ -21,5 +22,29 @@ def article_detail(request, slug):
 	return render(request, 'article_detail.html', {'article': article})
 
 def signup_view(request):
-	form = UserCreationForm()
+	if request.method == 'POST':
+		form = UserCreationForm(request.POST)
+		if form.is_valid():
+			user = form.save()
+			login(request, user)
+			return redirect('article_list')
+	else:
+		form = UserCreationForm()
 	return render(request, 'signup.html', {'form': form})
+
+def login_view(request):
+	if request.method == 'POST':
+		form = AuthenticationForm(data=request.POST)
+		if form.is_valid():
+			# TO DO: Log in the user 
+			user = form.get_user()
+			login(request, user)
+			return redirect('article_list')
+	else:
+		form = AuthenticationForm()
+	return render(request, 'login.html', {'form': form})
+
+def logout_view(request):
+	if request.method == 'POST':
+		logout(request)
+		return redirect('article_list')
