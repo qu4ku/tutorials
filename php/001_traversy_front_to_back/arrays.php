6d3e197a421aeb70cfd4c110c7909b808ccbaed3
qu4ku <qu4ku@hotmail.com>
2018-05-19 02:19:31 +0200

    php

---
+++ php/001_traversy_front_to_back/arrays.php
@@ -0,0 +1,18 @@
+<?php 
+	# Indexed
+	$people = array('kevin', 'martin', 'kamil');
+	// echo $people[1];
+	$ids = array(23, 45, 320);
+	// echo $ids[2];
+	$cars = ['toyota', 'nissan', 'mercedes'];
+	// echo $cars[1];
+	$cars[3] = 'chevy';
+	// echo $cars[3];
+	$cars[] = 'bmw';
+	// echo $cars[4];
+	// echo count($cars)
+	// print_r($cars)
+	// var_dump($cars)
+	$people = array('brad' => 34, 'kamil' => 40);
+	print_r($people);
+?>
---
+++ php/001_traversy_front_to_back/variables.php
@@ -0,0 +1,16 @@
+<?php
+	# Comment 
+	print 'Hello World';
+
+	$output = 'Hello Variable!';
+	$float1 = 4.4;
+	$float2 = 2.2;
+	$floatsum = $float1 + $float2;
+	$greeting = 'Hello' . ' ' . 'Kamil'.'!';
+	echo $output;
+	print $floatsum;
+	print $float1 + $float2;
+	print $greeting;
+	define('CONSTANT_GREETING', 'Hello Everyone!');
+	echo CONSTANT_GREETING;
+?>
