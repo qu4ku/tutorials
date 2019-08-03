3a54d039f173b6b783095cf5e9b2ba50571ccaed
qu4ku <qu4ku@hotmail.com>
2019-08-03 11:40:05 +0200

    react+django

--- react/003-react-django-justdjango/frontend/gui/src/App.js
+++ react/003-react-django-justdjango/frontend/gui/src/App.js
@@ -1,16 +1,19 @@
 import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
+import BaseRouter from './routes';
 import 'antd/dist/antd.css'; // or 'antd/dist/antd.less'
 
 import CustomLayout from './containers/Layout'
-import ArticleList from './containers/ArticleListView'
 
 
 function App() {
   return (
     <div className="App">
-      <CustomLayout>
-        <ArticleList />
-      </CustomLayout>
+      <Router>
+        <CustomLayout>
+          <BaseRouter />
+        </CustomLayout>
+       </Router>
     </div>
   );
 }
